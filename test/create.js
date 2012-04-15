7669935282b1f1ce03f1f0fdbd60929ca734fdbb
Chad Phillips <chad@apartmentlines.com>
2012-04-15 16:37:31 -0700

    round out tests for create method.

--- test/create.js
+++ test/create.js
@@ -1,10 +1,24 @@
+var _ = require('underscore');
 var db_pool = require('../../db-query-assistant');
 
-describe('create', function(){
-  describe('no database and pool arguments', function(){
-    it('should create a pool object with the default arguments', function() {
+var valid_pool_object = function(db) {
+  return _.isObject(db) && _.isObject(db.pool) && _.isFunction(db.query) && _.isFunction(db.querySeries) && _.isFunction(db.queryTransaction);
+}
+
+describe('#create()', function(){
+  describe('with no database and pool arguments', function(){
+    it('should return a valid pool object', function() {
       var db = db_pool.create('dummy');
-      db.should.be.a('object');
+      var valid_object = valid_pool_object(db);
+      valid_object.should.be.true;
     });
-  })
+  });
+  describe('with database and pool arguments', function(){
+    it('should return a valid pool object', function() {
+      var config = require('./config.dummy');
+      var db = db_pool.create('dummy', config.db, config.db_pool);
+      var valid_object = valid_pool_object(db);
+      valid_object.should.be.true;
+    });
+  });
 });
