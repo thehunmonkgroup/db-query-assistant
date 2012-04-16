79446343a660d30d51d117d25d0edd532b8b0c4d
Chad Phillips <chad@apartmentlines.com>
2012-04-16 15:56:34 -0700

    add common query testing functions to util lib.

--- test/util.js
+++ test/util.js
@@ -11,8 +11,28 @@ var get_driver = function() {
   return db;
 }
 
+var query_string = function() {
+  return "string";
+}
+
+var query_array = function() {
+  return ["array"];
+}
+
+var query_object = function(query) {
+  return query.sql("object");
+}
+
+var query_false = function(query) {
+  return false;
+}
+
 module.exports = {
   get_default_driver: get_default_driver,
   get_driver: get_driver,
+  query_string: query_string,
+  query_array: query_array,
+  query_object: query_object,
+  query_false: query_false,
 }
 
