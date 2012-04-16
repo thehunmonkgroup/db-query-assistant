3af2d1583f37ec5e20da063112e91a7bad3d2903
Chad Phillips <chad@apartmentlines.com>
2012-04-15 21:55:30 -0700

    refactor create tests, one per attribute inspected.

--- test/create.js
+++ test/create.js
@@ -1,24 +1,51 @@
 var _ = require('underscore');
 var db_pool = require('../../db-query-assistant');
+var config = require('./config.dummy');
 
-var valid_pool_object = function(db) {
-  return _.isObject(db) && _.isObject(db.pool) && _.isFunction(db.query) && _.isFunction(db.querySeries) && _.isFunction(db.queryTransaction);
+var get_default_driver = function() {
+  var db = db_pool.create('dummy');
+  return db;
 }
 
-describe('#create()', function(){
-  describe('with no database and pool arguments', function(){
-    it('should return a valid pool object', function() {
-      var db = db_pool.create('dummy');
-      var valid_object = valid_pool_object(db);
-      valid_object.should.be.true;
+var get_driver = function() {
+  var db = db_pool.create('dummy', config.db, config.db_pool);
+  return db;
+}
+
+describe('#create()', function() {
+  describe('with no database and pool arguments', function() {
+    it('should return pool with a pool object', function() {
+      get_default_driver().pool.should.be.a('object');
+    });
+    it('should return pool with a driver object', function() {
+      get_default_driver().driver.should.be.a('object');
+    });
+    it('should return pool with a query function', function() {
+      get_default_driver().query.should.be.a('function');
+    });
+    it('should return pool with a querySeries function', function() {
+      get_default_driver().querySeries.should.be.a('function');
+    });
+    it('should return pool with a queryTransaction function', function() {
+      get_default_driver().queryTransaction.should.be.a('function');
     });
   });
-  describe('with database and pool arguments', function(){
-    it('should return a valid pool object', function() {
-      var config = require('./config.dummy');
-      var db = db_pool.create('dummy', config.db, config.db_pool);
-      var valid_object = valid_pool_object(db);
-      valid_object.should.be.true;
+  describe('with database and pool arguments', function() {
+    it('should return pool with a pool object', function() {
+      get_driver().pool.should.be.a('object');
+    });
+    it('should return pool with a driver object', function() {
+      get_driver().driver.should.be.a('object');
+    });
+    it('should return pool with a query function', function() {
+      get_driver().query.should.be.a('function');
+    });
+    it('should return pool with a querySeries function', function() {
+      get_driver().querySeries.should.be.a('function');
+    });
+    it('should return pool with a queryTransaction function', function() {
+      get_driver().queryTransaction.should.be.a('function');
     });
   });
 });
+
