5fbf4736d5e29bd51110a4c78f0dfaf910d54db4
Chad Phillips <chad@apartmentlines.com>
2012-04-16 20:08:16 -0700

    round out the unit tests for single queries using the pool.query method.

--- test/query.js
+++ test/query.js
@@ -9,10 +9,9 @@ describe('pool#query()', function() {
   beforeEach(function() {
     db.driver.clear_query_stack();
   });
-  describe('with one successful query', function() {
+  describe('with one successful query (custom)', function() {
     it('should not return an error object', function(done) {
       var query = util.query_string;
-      var query_data = query();
       var cb = function(err, data) {
         should.not.exist(err);
         done();
@@ -24,6 +23,62 @@ describe('pool#query()', function() {
       var query_data = query();
       var cb = function(err, data) {
         data.query.should.equal(query_data);
+        done();
+      }
+      db.query(query, cb);
+    });
+  });
+  describe('with one successful query (object)', function() {
+    it('should not return an error object', function(done) {
+      var query = util.query_object;
+      var cb = function(err, data) {
+        should.not.exist(err);
+        done();
+      }
+      db.query(query, cb);
+    });
+    it('should return query data to the callback', function(done) {
+      var query = util.query_object;
+      var query_data = "object";
+      var cb = function(err, data) {
+        data.query.should.equal(query_data);
+        done();
+      }
+      db.query(query, cb);
+    });
+  });
+  describe('with one failed query', function() {
+    it('should return an error object', function(done) {
+      var query = util.query_error;
+      var query_data = query();
+      var cb = function(err, data) {
+        err.should.equal(query_data);
+        done();
+      }
+      db.query(query, cb);
+    });
+    it('should not return a data object', function(done) {
+      var query = util.query_error;
+      var cb = function(err, data) {
+        should.not.exist(data);
+        done();
+      }
+      db.query(query, cb);
+    });
+  });
+  describe('with one query returning false', function() {
+    it('should not return an error object', function(done) {
+      var query = util.query_false;
+      var cb = function(err, data) {
+        should.not.exist(err);
+        done();
+      }
+      db.query(query, cb);
+    });
+    it('should return data as false', function(done) {
+      var query = util.query_false;
+      var cb = function(err, data) {
+        data.should.be.false;
         done();
       }
       db.query(query, cb);
